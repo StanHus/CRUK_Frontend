110f72628f0055793e4abbf7ac2b8161d53ea370
StanHus <guseletovS@gmail.com>
2021-10-10 18:37:06 +0100

    css on help page

---
+++ src/components/Help/Main_Page_help.tsx
@@ -0,0 +1,50 @@
+import { Fragment } from "react";
+import "../../css/help.css";
+
+export default function Help() {
+  return (
+    <Fragment>
+      <h2 className="helpMessage">You can help!!!</h2>
+      <div className="helpOptions">
+        <button
+          className="helpOption"
+          onClick={() =>
+            window.open(
+              "https://www.cancerresearchuk.org/get-involved/volunteer"
+            )
+          }
+        >
+          Volunteer
+        </button>
+        <button
+          className="helpOption"
+          onClick={() =>
+            window.open(
+              "https://www.cancerresearchuk.org/get-involved/do-your-own-fundraising"
+            )
+          }
+        >
+          Do your own funraising
+        </button>
+        <button
+          className="helpOption"
+          onClick={() =>
+            window.open(
+              "https://www.cancerresearchuk.org/get-involved/leave-a-legacy-gift-in-your-will"
+            )
+          }
+        >
+          Leave a legacy gift in your Will
+        </button>
+        <button
+          className="helpOption"
+          onClick={() =>
+            window.open("https://www.cancerresearchuk.org/get-involved/donate")
+          }
+        >
+          Donate
+        </button>
+      </div>
+    </Fragment>
+  );
+}
