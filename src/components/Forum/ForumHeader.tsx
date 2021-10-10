ee425429b68b4349c8f5694833e8ccef40d094db
StanHus <guseletovS@gmail.com>
2021-10-10 20:53:38 +0100

    minor spelling mistake

--- src/components/Forum/ForumHeader.tsx
+++ src/components/Forum/ForumHeader.tsx
@@ -3,10 +3,10 @@ import "../../css/forum.css";
 export default function Header() {
   return (
     <div className="forumHeader">
-      <h1>Share what you you have been up too!</h1>
+      <h1>Share what you you have been up to!</h1>
       <h4>
-        <i>Inspire</i> others with your contributions, or get inspired
-        yourself)). All on this page
+        <i>Inspire</i> others with your contributions, or get inspired yourself.
+        All on this page
       </h4>
     </div>
   );
